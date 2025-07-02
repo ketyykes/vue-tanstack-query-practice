12f489c1a1978aa59265f7edcb59b27ed0c3264a
danny <xi4869@gmail.com>
2025-07-02 21:27:25 +0800

    feat: 在 SearchFilter 元件中新增搜尋功能，包含搜尋按鈕及相關事件的處理，提升使用者搜尋書籍的便利性。

--- src/views/TanstackView/components/SearchFilter.vue
+++ src/views/TanstackView/components/SearchFilter.vue
@@ -1,37 +1,53 @@
 <script setup>
 /**
+ * 搜尋欄位的值
+ *
  * @type {import('vue').PropType<string>}
- * @description 搜尋欄位的值
  */
-defineProps({
+const props = defineProps({
   /** @type {import('vue').PropType<string>} */
   modelValue: { type: String, default: '' },
 })
 
 /**
+ * 輸入變更事件
+ *
  * @type {import('vue').EmitsOptions}
- * @description 輸入變更事件
  */
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue', 'search'])
 
 /**
+ * 處理輸入變更
+ *
  * @type {(e: Event) => void}
- * @description 處理輸入變更
  */
 const handleInput = (e) => {
   emit('update:modelValue', e.target.value)
+}
+
+const handleSearch = () => {
+  emit('search', props.modelValue)
 }
 </script>
 
 <template>
   <div class="mb-5">
-    <input
-      type="text"
-      placeholder="搜尋書籍標題..."
-      :value="modelValue"
-      @input="handleInput"
-      class="min-w-[300px] rounded border border-gray-300 px-3 py-2"
-    />
+    <form @submit.prevent="handleSearch">
+      <input
+        type="text"
+        id="search"
+        name="search"
+        placeholder="搜尋書籍標題..."
+        :value="modelValue"
+        @input="handleInput"
+        class="min-w-[300px] rounded border border-gray-300 px-3 py-2"
+      />
+      <button
+        class="ml-8 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+      >
+        搜尋
+      </button>
+    </form>
   </div>
 </template>
 
